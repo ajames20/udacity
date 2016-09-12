b61134759f128cc67df94ba076f2db12f041c7f2
Andrew James <ajames20@gmail.com>
2016-09-12 15:38:28 +0300

    js file updates

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -1,8 +1,27 @@
-var awesomeThoughts = " I am Andrew and I am Awesome!"
+var name = "Andrew James";
+var role = "Full Stack Dev";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-var funToughts = awesomeThoughts.replace("Awesome", "Fun")
+var bio = {
+  "name": "Andrew",
+  "age": 35,
+  "contact": {
+    "email":"you@youremail.com",
+    "mobile": "(555)555-5555",
 
-console.log(awesomeThoughts);
-console.log(funToughts);
+  },
+  "biopic": "images/me.jpg"
+}
 
-$("#main").append(funToughts);
+
+var email = HTMLemail.replace("%data%", bio.contact.email);
+var phone = HTMLmobile.replace("%data%", bio.contact.mobile);
+var biopic = HTMLbioPic.replace("%data%", bio.biopic);
+
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
+$('#lets-connect').append(email);
+$('#lets-connect').append(phone);
+$('#main').append(biopic);
+//
