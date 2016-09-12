d2e1a75c8522c4d644e793c10f42b1d1d5a0d1de
Andrew James <ajames20@gmail.com>
2016-09-12 11:38:09 +0300

    first commit

---
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -0,0 +1,13 @@
+
+//$("#main").append(["Andrew"]);
+
+
+
+var awesomeThoughts = " I am Andrew and I am Awesome!"
+
+var funToughts = awesomeThoughts.replace("Awesome", "Fun")
+
+console.log(awesomeThoughts);
+console.log(funToughts);
+
+$("#main").append(funToughts);
